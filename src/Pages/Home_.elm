f827a0c6bc4e1a49d52b9c7b60ffbeb3da9b0c15
Rafael Medeiros Gomes <devmedeiros@pm.me>
2023-02-02 10:16:34 -0300

    adds elm-explorations/markdown lib

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,11 +1,35 @@
 module Pages.Home_ exposing (page)
 
-import Html
+import Html exposing (Html)
+import Html.Attributes exposing (class)
+import Markdown
 import View exposing (View)
 
 
 page : View msg
 page =
     { title = "Homepage"
-    , body = [ Html.text "Hello, world!" ]
+    , body = [ content ]
+    }
+
+
+content : Html msg
+content =
+    Markdown.toHtmlWith options [ class "content" ] """
+
+# Todo list
+
+  1. [x] Test markdown to HTML lib
+  2. [ ] Add dynamic route for articles
+  3. [ ] Add elm-frontmatter
+  4. [ ] Add articles as .md files
+
+"""
+
+
+options =
+    { githubFlavored = Just { tables = True, breaks = True }
+    , defaultHighlighting = Nothing
+    , sanitize = True
+    , smartypants = False
     }
